c0be94cd1ad950df11a1334ddad5185295a60668
Macktireh <abdimack97@gmail.com>
2022-07-09 22:58:15 +0200

    fix: The SECRET_KEY setting must not be empty

--- config/settings/base.py
+++ config/settings/base.py
@@ -12,7 +12,7 @@ load = load_dotenv(os.path.join(BASE_DIR, '.env'))
 ENV = os.environ.get('ENV', 'development')
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'spifgucrnt)heocnegcfi(zye624_2984985@54sd9+fv4-r9v4d9gv4s9r8f4vc9rd4g98')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = ENV != 'production'
