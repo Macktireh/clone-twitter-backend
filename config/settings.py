95951dec955481289f8a93518825e9cca31f7101
Macktireh <abdimack97@gmail.com>
2023-04-05 19:58:37 +0200

    fix staticfiles

--- config/settings.py
+++ config/settings.py
@@ -199,7 +199,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
