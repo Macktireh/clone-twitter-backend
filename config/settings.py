f1ac26e2c51882920edfef8a77f044aab1062618
Macktireh <abdimack97@gmail.com>
2023-10-27 16:22:43 +0200

    Fix bug when updating tweet and update dependencies

--- config/settings.py
+++ config/settings.py
@@ -251,11 +251,11 @@ AUTH_USER_MODEL = "authentication.User"
 
 # Config Send Email
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = os.environ["EMAIL_HOST"]
-EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
-EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
-EMAIL_PORT = os.environ["EMAIL_PORT"]
-EMAIL_USE_TLS = os.environ["EMAIL_USE_TLS"]
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
 
 
 # settings JWT
