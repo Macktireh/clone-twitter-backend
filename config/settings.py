1f45c50dfaca5249f00f8f7469ac60adbab1f61e
Macktireh <abdimack97@gmail.com>
2024-07-21 21:40:32 +0200

    add default SECRET_KEY

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ load = load_dotenv(os.path.join(BASE_DIR, ".env"))
 ENV = os.environ.get("ENV", "development")
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", "sfghdtjrfypjouis5414enrgizn51454gpoze65s6pgptnoiuzenpfoiqerzn")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = ENV != "production"
